bd200e796706115197459ffb427a76c5ffe579e1
lauroolim <lauroolimota743@gmail.com>
2024-11-14 00:38:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'urbanspotter-mobile',
+  webDir: 'www'
+};
+
+export default config;
